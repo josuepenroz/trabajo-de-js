91acda70b41f1082e5067cc08d400f55da530bec
josue penroz <josue.penroz.maldonado@alumnos.sip.cl>
2025-11-05 19:55:05 -0400

    tranajo terminado

---
+++ pre-sertificacion/JavaScript.js
@@ -0,0 +1,46 @@
+function MostrarAlerta(){
+    alert("Gracias por donar a  AdoptaTuÁrbol!!")
+}
+/* function de adoptar  */
+function adoptar() {
+let boton = document.querySelector(".boton-adoptar");
+if(boton.innerText === "Adoptar") {
+    boton.innerText = "Adoptado";
+    boton.classList.add("adoptado")
+} else {
+    boton.innerText = "Adoptar"
+    boton.classList.remove("adoptado")
+}
+}
+function adoptar1() {
+let boton = document.querySelector(".boton-adoptar1");
+if(boton.innerText === "Adoptar") {
+    boton.innerText = "Adoptado";
+    boton.classList.add("adoptado")
+} else {
+    boton.innerText = "Adoptar"
+    boton.classList.remove("adoptado")
+}
+}
+
+function adoptar2() {
+let boton = document.querySelector(".boton-adoptar2");
+if(boton.innerText === "Adoptar") {
+    boton.innerText = "Adoptado";
+    boton.classList.add("adoptado")
+} else {
+    boton.innerText = "Adoptar"
+    boton.classList.remove("adoptado")
+}
+}
+
+function adoptar3() {
+let boton = document.querySelector(".boton-adoptar3");
+if(boton.innerText === "Adoptar") {
+    boton.innerText = "Adoptado";
+    boton.classList.add("adoptado")
+} else {
+    boton.innerText = "Adoptar"
+    boton.classList.remove("adoptado")
+}
+}
